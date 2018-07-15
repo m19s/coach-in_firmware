79e1e1cba592f38bdcd962156c50eed53d0cba94
Akira Matsuda <akira.matsuda@me.com>
2018-07-15 15:31:45 +0900

    [update] [esp] パケットをパースする

--- esp32/main/main.cpp
+++ esp32/main/main.cpp
@@ -18,15 +18,18 @@ using namespace m2d::ESP32;
 void app_main()
 {
 	static Logger::Group logger("coach_in");
+	logger.debug << "coach-in start" << Logger::endl;
 	static m2d::FreeRTOS::Task task("EMS task", 10, 1024 * 3, [&] {
 		SPIWrapper spi(4000000, 3, (gpio_num_t)GPIO_NUM_18, (gpio_num_t)GPIO_NUM_23, (gpio_num_t)GPIO_NUM_19, (gpio_num_t)GPIO_NUM_5, VSPI_HOST, (SPI_DEVICE_TXBIT_LSBFIRST | SPI_DEVICE_TXBIT_LSBFIRST));
+
+		int i = 0;
 		while (1) {
 			// 0b000 00001 0001 0001
 			m19s::coach_in::ESP32::Packet packet;
 			packet.channel_identifier = 0;
-			packet.pulse = 50;
-			packet.frequency = 60;
-			packet.duration = 600;
+			packet.pulse = 50 + i * 10;
+			packet.frequency = 60 + i * 10;
+			packet.duration = 600 + i * 100;
 
 			auto v = packet.to_byte_vector();
 			uint8_t *data = &v[0];
@@ -40,6 +43,8 @@ void app_main()
 			t.set_tx_buffer(&data[1], 1);
 			assert(spi.transmit(t) == ESP_OK);
 
+			i++;
+			i %= 10;
 			vTaskDelay(3000 / portTICK_PERIOD_MS);
 		}
 	});
